8cb18c6602587ca43c67e3b601073c99dc08319a
shane_mansueto <shanemansueto13@gmail.com>
2025-05-13 21:34:53 +0800

    test run - 1

---
+++ faculty/classes.js
@@ -0,0 +1,149 @@
+const contentSection = document.querySelector(".content-section")
+const contentLabel = document.querySelector(".content-label")
+const contentItems = document.querySelectorAll(".content-item")
+const contentButton = document.querySelector(".content-button")
+const inputButton = document.createElement("button")
+inputButton.className = "input-button"
+inputButton.innerHTML = "+"
+let contentFlip = 0
+
+
+
+let inputCount = 0
+
+const addInput = () => {
+		inputButton.insertAdjacentHTML("beforebegin", `
+				<div class="input-wrapper">
+						<label for="user_id${inputCount}">Student${inputCount + 1} ID:</label>
+						<textarea class="content-input" id="user_id${inputCount}" type="text"></textarea>
+						<label for="username${inputCount}">Student${inputCount + 1} Name:</label>
+						<textarea class="content-input" id="username${inputCount}" type="text"></textarea>
+				</div>
+				`)
+		inputCount++
+}
+
+inputButton.onclick = addInput
+
+function formatContent() {
+		for (var i = 0; i < localStorage.length; i++) {
+				let item = JSON.parse(localStorage.getItem(localStorage.key(i)))
+				let formatedItem = document.createElement("a")
+				let formatedElement1 = document.createElement("span")
+				let formatedElement2 = document.createElement("span")
+				let formatedElement3 = document.createElement("span")
+				formatedItem.className = "content-item"
+				formatedElement1.className = "content-element"
+				formatedElement2.className = "content-element"
+				formatedElement3.className = "content-element"
+				formatedElement1.innerHTML = item.course
+				formatedElement2.innerHTML = item.group_number
+				formatedElement3.innerHTML = item.class_list.length
+				formatedItem.append(formatedElement1)
+				formatedItem.append(formatedElement2)
+				formatedItem.append(formatedElement3)
+				contentSection.append(formatedItem)
+				formatedItem.onclick = () => {
+						contentSection.innerHTML = ""
+						contentSection.classList.add("show-content")
+						formatedElement1.innerHTML = "Course: " + formatedElement1.innerHTML
+						formatedElement2.innerHTML = "Group Number: " + formatedElement2.innerHTML
+						contentSection.append(formatedElement1)
+						contentSection.append(formatedElement2)
+						item.class_list.forEach(user => {
+								let formatedElement1 = document.createElement("span")
+								let formatedElement2 = document.createElement("span")
+								let formatedElement3 = document.createElement("span")
+								formatedElement1.className = "content-element"
+								formatedElement2.className = "content-element"
+								formatedElement1.innerHTML = user.user_id
+								formatedElement2.innerHTML = user.username
+								formatedElement3.innerHTML = user.username + "\n"
+								contentSection.append(formatedElement1)
+								contentSection.append(formatedElement2)
+								contentSection.append(formatedElement3)
+						})
+						contentButton.innerHTML = "Return to Menu"
+						contentButton.onclick = () => {
+								contentSection.innerHTML = ""
+								contentSection.classList.remove("show-content")
+								contentSection.append(contentLabel)
+								contentButton.onclick = contentFlipper
+								contentButton.innerHTML = "Add Content"
+								formatContent()
+						}
+				}
+//				contentSection.insertAdjacentHTML("beforeend", `
+//						<a class="content-item">
+//								<span class="content-element">
+//										${item.course}
+//								</span>
+//								<span class="content-element">
+//										${item.title}
+//								</span>
+//						</a>
+//						`)
+		}
+}
+
+const addContent = () => {
+		inputCount = 0
+		contentSection.innerHTML = ""
+		contentSection.classList.add("form-section")
+		contentSection.insertAdjacentHTML("beforeend", `
+				<label for="course">Course Code: </label>
+				<input class="input-label" type="text" id="course"/>
+				<label for="group-number">Group Number: </label>
+				<input class="input-label" type="text" id="group-number"/>
+				`)
+		contentSection.append(inputButton)
+		addInput()
+		addInput()
+		addInput()
+}
+
+const submitContent = () => {
+		let label1 = document.querySelector("#course")
+		let label2 = document.querySelector("#group-number")
+		let classList = []
+		for (var i = 0; i < inputCount; i++) {
+				let element1 = document.querySelector(`#username${i}`)
+				classList.push({username:element1.value})
+		}
+		localStorage.setItem(label2.value, JSON.stringify({"course":label1.value, "group_number":label2.value, "class_list":classList}))
+		localStorage.removeItem("")
+
+		contentSection.innerHTML = ""
+		contentSection.classList.remove("form-section")
+		contentSection.append(contentLabel)
+		contentItems.forEach(item => {
+				contentSection.append(item)
+		})
+				
+		formatContent()
+}
+
+async function initialize(){
+	await fetch("https://shanemansueto.github.io/web_dev_class/resources/placeholder_classes.json")
+	.then(res => res.json())
+	.then(json => {
+			json.forEach(item => {
+					localStorage.setItem(item.title, JSON.stringify(item))
+			})
+	})
+	formatContent()
+}
+initialize()
+const contentFlipper = () => {
+		if (contentFlip) {
+				submitContent()
+				contentButton.innerHTML = "Add Content"
+		}
+		else {
+				addContent()
+				contentButton.innerHTML = "Submit Content"
+		}
+		contentFlip = !contentFlip
+}
+
+contentButton.onclick = contentFlipper
---
+++ faculty/resources.js
@@ -0,0 +1,147 @@
+const contentSection = document.querySelector(".content-section")
+const contentLabel = document.querySelector(".content-label")
+const contentItems = document.querySelectorAll(".content-item")
+const contentButton = document.querySelector(".content-button")
+const inputButton = document.createElement("button")
+inputButton.className = "input-button"
+inputButton.innerHTML = "+"
+let contentFlip = 0
+
+
+
+let inputCount = 0
+
+const addInput = () => {
+		inputButton.insertAdjacentHTML("beforebegin", `
+				<div class="input-wrapper">
+						<label for="question${inputCount}">Q${inputCount + 1}</label>
+						<br/>
+						<br/>
+						<textarea class="content-input question" id="question${inputCount}" type="text"></textarea>
+						<br/>
+						<label for="answer${inputCount}">answer: </label>
+						<input class="content-input answer" id="answer${inputCount}" type="text"/>
+				</div>
+				`)
+		inputCount++
+}
+
+inputButton.onclick = addInput
+
+function formatContent() {
+		for (var i = 0; i < localStorage.length; i++) {
+				let item = JSON.parse(localStorage.getItem(localStorage.key(i)))
+				let formatedItem = document.createElement("a")
+				let formatedElement1 = document.createElement("span")
+				let formatedElement2 = document.createElement("span")
+				formatedItem.className = "content-item"
+				formatedElement1.className = "content-element"
+				formatedElement2.className = "content-element"
+				formatedElement1.innerHTML = item.course
+				formatedElement2.innerHTML = item.title
+				formatedItem.append(formatedElement1)
+				formatedItem.append(formatedElement2)
+				contentSection.append(formatedItem)
+				formatedItem.onclick = () => {
+						contentSection.innerHTML = ""
+						contentSection.classList.add("show-content")
+						formatedElement1.innerHTML = "Course: " + formatedElement1.innerHTML
+						formatedElement2.innerHTML = "Title: " + formatedElement2.innerHTML
+						contentSection.append(formatedElement1)
+						contentSection.append(formatedElement2)
+						item.problems.forEach(problem => {
+								let formatedElement1 = document.createElement("span")
+								let formatedElement2 = document.createElement("span")
+								formatedElement1.className = "content-element"
+								formatedElement2.className = "content-element"
+								formatedElement1.innerHTML = problem.question
+								formatedElement2.innerHTML = problem.answer
+								formatedElement2.style = "color:red"
+								contentSection.append(formatedElement1)
+								contentSection.append(formatedElement2)
+						})
+						contentButton.innerHTML = "Return to Menu"
+						contentButton.onclick = () => {
+								contentSection.innerHTML = ""
+								contentSection.classList.remove("show-content")
+								contentSection.append(contentLabel)
+								contentButton.onclick = contentFlipper
+								contentButton.innerHTML = "Add Content"
+								formatContent()
+						}
+				}
+//				contentSection.insertAdjacentHTML("beforeend", `
+//						<a class="content-item">
+//								<span class="content-element">
+//										${item.course}
+//								</span>
+//								<span class="content-element">
+//										${item.title}
+//								</span>
+//						</a>
+//						`)
+		}
+}
+
+const addContent = () => {
+		inputCount = 0
+		contentSection.innerHTML = ""
+		contentSection.classList.add("form-section")
+		contentSection.insertAdjacentHTML("beforeend", `
+				<label for="course">Course Code: </label>
+				<input class="input-label" type="text" id="course"/>
+				<label for="title">Title: </label>
+				<input class="input-label" type="text" id="title"/>
+				`)
+		contentSection.append(inputButton)
+		addInput()
+		addInput()
+		addInput()
+}
+
+const submitContent = () => {
+		let label1 = document.querySelector("#course")
+		let label2 = document.querySelector("#title")
+		let problems = []
+		for (var i = 0; i < inputCount; i++) {
+				let element1 = document.querySelector(`#question${i}`)
+				let element2 = document.querySelector(`#answer${i}`)
+				problems.push({question:element1.value,answer:element2.value})
+		}
+		localStorage.setItem(label2.value, JSON.stringify({"course":label1.value, "title":label2.value, "problems":problems}))
+		localStorage.removeItem("")
+
+		contentSection.innerHTML = ""
+		contentSection.classList.remove("form-section")
+		contentSection.append(contentLabel)
+		contentItems.forEach(item => {
+				contentSection.append(item)
+		})
+				
+		formatContent()
+}
+
+async function initialize(){
+	await fetch("https://shanemansueto.github.io/web_dev_class/resources/placeholder_questionares.json")
+	.then(res => res.json())
+	.then(json => {
+			json.forEach(item => {
+					localStorage.setItem(item.title, JSON.stringify(item))
+			})
+	})
+	formatContent()
+}
+initialize()
+const contentFlipper = () => {
+		if (contentFlip) {
+				submitContent()
+				contentButton.innerHTML = "Add Content"
+		}
+		else {
+				addContent()
+				contentButton.innerHTML = "Submit Content"
+		}
+		contentFlip = !contentFlip
+}
+
+contentButton.onclick = contentFlipper
